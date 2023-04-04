e1786b006ae7dbde2a360e27326265c48b6b8c00
Mario Toffia <mario.toffia@gmail.com>
2023-04-04 10:09:40 +0200

    fixed bug in abs.go

---
+++ amath/abs.go
@@ -0,0 +1,22 @@
+package vector
+
+import (
+	"math"
+
+	"github.com/mariotoffia/goannoy/vector"
+)
+
+// Abs32 is the same as math.Abs, but for float32.
+func Abs32(x float32) float32 {
+	return math.Float32frombits(math.Float32bits(x) &^ (1 << 31))
+}
+
+// Abs is less performant on hardware where floating point operations are
+// expensive, but it is generic.
+func Abs[T vector.Calculable](x T) T {
+	if x < 0 {
+		return -x
+	}
+
+	return x
+}
