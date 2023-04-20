a28917f01b8d11db03e1875b464b45a1799169b6
Mario Toffia <mario.toffia@gmail.com>
2023-04-20 06:44:48 +0200

    last take

--- platform/dot_neon.go
+++ platform/dot_neon.go
@@ -8,11 +8,8 @@ func init() {
 }
 
 //go:noescape
-func neonDotProductF32(result *float32, x, y *float32, f uint32)
+func neonDotProductF32(v1, v2 *float32, length int) float32
 
 func dotF32Neon(x, y *float32, vectorLength uint32) float32 {
-
-	var result float32
-	neonDotProductF32(&result, x, y, vectorLength)
-	return result
+	return neonDotProductF32(x, y, int(vectorLength))
 }
--- platform/dot_neon.s
+++ platform/dot_neon.s
@@ -4,63 +4,34 @@
 
 #include "textflag.h"
 
-// void neonDotProductF32(float* result, const float* x, const float *y, int f)
-TEXT ·neonDotProductF32(SB), NOSPLIT, $0-32
-    // Save registers
-    STP X29, X30, [SP, #-16]!
-    MOV X29, SP
+// func DotProduct(v1, v2 *float32, length int) float32
+TEXT ·neonDotProductF32(SB), NOSPLIT, $0-28
+    MOVD R0, R0   // Load the first vector pointer (v1) into x0
+    MOVD R1, R1   // Load the second vector pointer (v2) into x1
+    MOVD R2, R2   // Load the vector length into w2
 
-    // Load arguments
-    MOV X8, result+0(FP)
-    MOV X9, x+8(FP)
-    MOV X10, y+16(FP)
-    MOV W11, f+24(FP)
+    FMOVS F0, ZR // Initialize the result (dot product) to 0
 
-    // Initialize dot product register
-    FMOV V0.4S, #0.0
+    // Neon loop
+    CBZ R2, done
+loop:
+    MOVD.P 16(R0), V1 // Load 4 float32 values from v1
+    MOVD.P 16(R1), V2 // Load 4 float32 values from v2
 
-    // Main loop for dot product
-DotProductLoop:
-    CMP W11, #3
-    BLS DotProductRemaining
+    FMULS V3.S[0], V1.S[0], V2.S[0] // Multiply v1 and v2 element-wise
+    FMULS V3.S[1], V1.S[1], V2.S[1]
+    FMULS V3.S[2], V1.S[2], V2.S[2]
+    FMULS V3.S[3], V1.S[3], V2.S[3]
 
-    // Load 4 values from x and y and perform FMA
-    LD1 {V1.4S}, [X9], #16
-    LD1 {V2.4S}, [X10], #16
-    FMLA V0.4S, V1.4S, V2.4S
+    FADDS F1, V3.S[0], V3.S[1]
+    FADDS F1, F1, V3.S[2]
+    FADDS F1, F1, V3.S[3]
 
-    // Decrement f by 4
-    SUB W11, W11, #4
+    FADDS F0, F0, F1          // Add the sum to the result (dot product)
 
-    B DotProductLoop
+    SUBS R2, R2, $4          // Decrement the length by 4
+    BGT loop                 // Continue loop if there are more elements
 
-DotProductRemaining:
-    // Calculate the horizontal sum of the dot product register
-    FADD V0.4S, V0.4S, V0.S[1]
-    FADD V0.4S, V0.4S, V0.S[2]
-    FADD V0.4S, V0.4S, V0.S[3]
-
-    // Remaining values loop
-RemainingLoop:
-    CBZ W11, Done
-
-    // Load x and y values
-    LD1 {V1.S}[0], [X9], #4
-    LD1 {V2.S}[0], [X10], #4
-
-    // Multiply and add to the result
-    FMLA V0.S, V1.S, V2.S
-
-    // Decrement f
-    SUB W11, W11, #1
-
-    B RemainingLoop
-
-Done:
-    // Store the final result
-    STR S0, [X8]
-
-    // Restore registers and return
-    MOV SP, X29
-    LDP X29, X30, [SP], #16
-    RET
+done:
+    FMOVS F0, F0 // Return the result (dot product) in f0
+    RET         // Return
