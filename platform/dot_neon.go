3108ebdf1ef3fc661f4e4ab47c47c1a8cd21c425
Mario Toffia <mario.toffia@gmail.com>
2023-05-08 07:52:23 +0200

    removed all x64 for now

--- platform/dot_neon.go
+++ platform/dot_neon.go
@@ -3,11 +3,13 @@
 
 package platform
 
+import _ "unsafe" // Required for go:linkname
+
 func init() {
 	DotProduct = dotF32Neon
 }
 
-//go:noescape
+//go:linkname neonDotProductF32 neonDotProductF32
 func neonDotProductF32(v1, v2 *float32, length uint32) float32
 
 func dotF32Neon(x, y *float32, vectorLength uint32) float32 {
--- platform/dot_neon.s
+++ platform/dot_neon.s
@@ -4,27 +4,33 @@
 
 #include "textflag.h"
 
-// func ·neonDotProductF32(v1, v2 *float32, vectorLength uint32) float32
-TEXT ·neonDotProductF32(SB),$0-20
-    MOV  v1.ptr(R0), X4       // X4 holds the pointer to v1
-    MOV  v2.ptr(R1), X5       // X5 holds the pointer to v2
-    MOV  vectorLength(R2), W3  // W3 holds the length of vectors
+// Function declaration
+TEXT ·neonDotProductF32(SB), NOSPLIT, $0-32
+  // Load vector pointers
+  MOVD v1+0(FP), R4
+  MOVD v2+8(FP), R5
 
-    MOVZ $0x0, W0              // W0 holds the index i
-    FMOV ZR, S2                // S2 holds the dot product result
+  // Load vector length
+  MOVD length+16(FP), R3
 
-loop:
-    CMP W0, W3
-    BGE done
+  // Prepare loop counter and result register
+  MOVD $0, R6
+  FMOVDZR F0
 
-    LDR S0, (X4)(W0*4)         // Load v1[i] into S0
-    LDR S1, (X5)(W0*4)         // Load v2[i] into S1
-    FMUL S0, S1, S0            // Multiply v1[i] * v2[i]
-    FADD S2, S0, S2            // Add the result to the dot product: dotProduct += v1[i] * v2[i]
+  // Loop
+neon_dot_product_loop:
+  // Load float32 values from v1 and v2
+  FMOVWU (R4)(R6*4), F1
+  FMOVWU (R5)(R6*4), F2
 
-    ADD W0, W0, $1             // Increment the index i
-    B   loop                   // Jump back to the loop
+  // Multiply and accumulate
+  FMADDS F1, F2, F0, F0
 
-done:
-    FMOV F0, S2                // Return the dot product result in F0
-    RET
+  // Increment loop counter and compare with the length
+  ADD $1, R6, R6
+  CMP R6, R3
+  BLT neon_dot_product_loop
+
+  // Store result and return
+  FMOVWDU F0, res+24(FP)
+  RET
